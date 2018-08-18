ae620389caac309e8b89b326e1809dc073b5d101
Geoffrey Nwachukwu <babajeffo@live.com>
2018-08-18 15:42:29 +0100

    Added feature for checking if a token has expired

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -1,22 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+    openDialog(): any {
+        console.log('Session has ended, please sign in again');
+    }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
-                }
-            });
-
-            console.log('Token Intercepted');
+        if (currentUser) {
+            const isExpired = this.isTokenExpired(currentUser.token);
+            if (!isExpired) {
+                request = request.clone({
+                    setHeaders: {
+                        Authorization: `Bearer ${currentUser.token}`
+                    }
+                });
+            } else {
+                this.openDialog();
+            }
         }
 
         return next.handle(request);
+    }
+
+    getToken () {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        return currentUser.token;
+    }
+
+    private getTokenExpirationDate(token: string): Date {
+        const decoded = jwt_decode(token);
+        if (decoded.exp === undefined) {return null; }
+
+        const date = new Date(0);
+        date.setUTCSeconds(decoded.exp);
+        console.log(date);
+        return date;
+    }
+
+    private isTokenExpired(token?: string): boolean {
+        if (!token) {token = this.getToken(); }
+        if (!token) {return true; }
+
+        const date = this.getTokenExpirationDate(token);
+        if (date === undefined) {return false; }
+        const answer = !(date.valueOf() > new Date().valueOf());
+        console.log(answer);
+        return !(date.valueOf() > new Date().valueOf());
     }
 }
