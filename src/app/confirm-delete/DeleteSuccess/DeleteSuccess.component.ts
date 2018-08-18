3c98434a0d93a70e7aafe5256b438df698c48d31
Geoffrey Nwachukwu <babajeffo@live.com>
2018-08-18 15:29:31 +0100

    Created a snackbar that confirms a marker was deleted

---
+++ src/app/confirm-delete/DeleteSuccess/DeleteSuccess.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-success',
+  templateUrl: './DeleteSuccess.component.html',
+  styleUrls: ['./DeleteSuccess.component.scss']
+})
+export class DeleteSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
